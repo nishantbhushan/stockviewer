17a2cc32ede373bb57ae17ea3cf71a25664c20de
Nishant Bhushan <nishantbhushan@gmail.com>
2023-01-04 14:32:42 -0500

    missed the dotenv inclusion into settings

--- stocks/settings.py
+++ stocks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sd-#3!o^02)4yc!rz5(w6o6g^7+#xddcqh37*%dj*qh9f$qwo1'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-sd-#3!o^02)4yc!rz5(w6o6g^7+#xddcqh37*%dj*qh9f$qwo1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))=='1'
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
